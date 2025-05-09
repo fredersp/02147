42d325c8803857faed038daaf332df0713b6d938
NicolineSimoneDTU <159460962+NicolineSimoneDTU@users.noreply.github.com>
2025-05-09 10:27:42 +0200

    Update Assignment4_part2.r
    plots

--- Assignment 4/Assignment4_part2.r
+++ Assignment 4/Assignment4_part2.r
@@ -1,2 +1,35 @@
-library(httpgd)
-hgd() 
+#library(httpgd)
+#hgd() 
+
+
+# Load necessary libraries
+library(ggplot2)
+library(tidyr)
+library(dplyr)
+
+setwd("/Users/nicolinesimonesachmann/Documents/DTU/Times Series Analysis/02147/Assignment 4")
+
+
+# If needed, load your dataset
+df <- read.csv("transformer_data.csv")
+
+
+
+
+# Check structure
+str(df)
+
+# Reshape for plotting
+df_long <- df %>%
+  mutate(time = 1:n()) %>%
+  pivot_longer(cols = c(Yt, Ta_t, Phi_s_t, Phi_I_t),
+               names_to = "variable", values_to = "value")
+
+# Plot all variables over time
+ggplot(df_long, aes(x = time, y = value, color = variable)) +
+  geom_line() +
+  facet_wrap(~ variable, scales = "free_y", ncol = 1) +
+  labs(title = "Exploratory Time Series Plots",
+       x = "Time (hours)",
+       y = "Value") +
+  theme_minimal()
